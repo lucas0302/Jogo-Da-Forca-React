8c7c74c1f01aa5d4f3405022e8e80b75ea2cfc34
Lucas Araujo <lucasao744@gmail.com>
2024-01-22 15:26:27 -0300

    criando o layout do jogo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import palavras from "./constants/palavras";
-import alfabeto from "./constants/alfabeto"
+import alfabeto from "./constants/alfabeto";
+import forca0 from "./assets/forca0.png";
 function App() {
-    return(
-        <div>
-        
+    return (
+        <div className="container-tela">
+            <div className="container-forca">
+                <img src={forca0} alt="forca" />
+                <button>Escolher palavra</button>
+                <h1>_ _ _ _ _ _ _</h1>
+            </div>
+            <div className="container-letras">
+                {alfabeto.map((letra) => <button key={letra}c disabled={true} >{letra}</button> )}
+            </div>
+
         </div>
     )
-    
+
 }
 export default App;
