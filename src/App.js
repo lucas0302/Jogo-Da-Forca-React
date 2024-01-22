d063fa7825690c6d40c9cd1ad8072ea2976950fb
Lucas Araujo <lucasao744@gmail.com>
2024-01-22 19:54:04 -0300

    criando as functions iniciarJogo e sortearPalavra

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import palavras from "./constants/palavras";
 import alfabeto from "./constants/alfabeto";
 import forca0 from "./assets/forca0.png";
+import forca1 from "./assets/forca1.png";
+import forca2 from "./assets/forca2.png";
+import forca3 from "./assets/forca3.png";
+import forca4 from "./assets/forca4.png";
+import forca5 from "./assets/forca5.png";
+import forca6 from "./assets/forca6.png";
+import {useState} from "react";
+
+const imagens =[forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 function App() {
+    const [erros, setErros]=useState(0);
+    const [palavraEscolhida, setPalavraEscolhida]= useState([]);
+    const [palavraJogo,setPalavraJogo] = useState([]);
+
+    function iniciarJogo(){
+        setErros(0);
+        sortearPalavra();
+    }
+
+    function sortearPalavra() {
+        const indice = Math.floor(Math.random() * palavras.length);
+        const palavra = palavras[indice];
+        const palavraArray = palavra.split("");
+        setPalavraEscolhida(palavraArray);
+        
+        let tracinhos =[];
+        palavraArray.forEach(()=> tracinhos.push(" _"));
+        setPalavraJogo(tracinhos);
+    }
+    
     return (
         <div className="container-tela">
             <div className="container-forca">
-                <img src={forca0} alt="forca" />
-                <button>Escolher palavra</button>
-                <h1>_ _ _ _ _ _ _</h1>
+                <img src={imagens[erros]} alt="forca" />
+                <button onClick={iniciarJogo}>Escolher palavra</button>
+                <h1>{palavraJogo}</h1>
             </div>
             <div className="container-letras">
                 {alfabeto.map((letra) => <button key={letra}c disabled={true} >{letra}</button> )}
