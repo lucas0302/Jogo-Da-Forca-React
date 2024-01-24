3a6ba55a72a8b5fb8361697613708edfdec68579
Lucas Araujo <lucasao744@gmail.com>
2024-01-24 17:25:19 -0300

    funcao de acertoletra e erroLetra

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import forca3 from "./assets/forca3.png";
 import forca4 from "./assets/forca4.png";
 import forca5 from "./assets/forca5.png";
 import forca6 from "./assets/forca6.png";
-import {useState} from "react";
+import { useState } from "react";
 
-const imagens =[forca0, forca1, forca2, forca3, forca4, forca5, forca6];
+const imagens = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 function App() {
-    const [erros, setErros]=useState(0);
-    const [palavraEscolhida, setPalavraEscolhida]= useState([]);
-    const [palavraJogo,setPalavraJogo] = useState([]);
+    const [erros, setErros] = useState(0);
+    const [palavraEscolhida, setPalavraEscolhida] = useState([]);
+    const [palavraJogo, setPalavraJogo] = useState([]);
+    const [letrasUsadas, setLetrasUsadas] = useState(alfabeto);
 
-    function iniciarJogo(){
+    function iniciarJogo() {
         setErros(0);
+        setLetrasUsadas([]);
         sortearPalavra();
     }
 
@@ -25,12 +27,36 @@ function App() {
         const palavra = palavras[indice];
         const palavraArray = palavra.split("");
         setPalavraEscolhida(palavraArray);
-        
-        let tracinhos =[];
-        palavraArray.forEach(()=> tracinhos.push(" _"));
+
+        let tracinhos = [];
+        palavraArray.forEach(() => tracinhos.push(" _ "));
         setPalavraJogo(tracinhos);
     }
-    
+
+    function clicarLetra(letra) {
+        setLetrasUsadas([...letrasUsadas, letra]);
+
+        if(palavraEscolhida.includes(letra)){
+            acertoletra(letra);
+        }else{
+            erroLetra(letra);
+        }
+    }
+
+    function acertoletra(letra) {
+        const novaPalavraJogo = [...palavraJogo];
+
+        palavraEscolhida.forEach((l, i)=> {
+            if(l === letra){
+                novaPalavraJogo[i] = letra;
+            }
+        });
+        setPalavraJogo(novaPalavraJogo);
+    }
+    function erroLetra(letra) {
+        const novosErros = erros + 1;
+        setErros(novosErros);
+    }
     return (
         <div className="container-tela">
             <div className="container-forca">
@@ -39,7 +65,9 @@ function App() {
                 <h1>{palavraJogo}</h1>
             </div>
             <div className="container-letras">
-                {alfabeto.map((letra) => <button key={letra}c disabled={true} >{letra}</button> )}
+                {alfabeto.map((letra) => <button key={letra}
+                    disabled={letrasUsadas.includes(letra)}
+                    onClick={() => clicarLetra(letra)}>{letra}</button>)}
             </div>
 
         </div>
