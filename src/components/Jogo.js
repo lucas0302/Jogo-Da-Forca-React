257a1a42a56cbb7f29e22918ec9e3fe55a4a625f
Lucas Araujo <lucasao744@gmail.com>
2024-01-31 17:20:36 -0300

    imgs

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,10 +1,10 @@
-import forca0 from "../assets/forca0.png";
-import forca1 from "../assets/forca1.png";
-import forca2 from "../assets/forca2.png";
-import forca3 from "../assets/forca3.png";
-import forca4 from "../assets/forca4.png";
-import forca5 from "../assets/forca5.png";
-import forca6 from "../assets/forca6.png";
+import forca0 from "../assets/forca0.jpg";
+import forca1 from "../assets/forca1.jpg";
+import forca2 from "../assets/forca2.jpg";
+import forca3 from "../assets/forca3.jpg";
+import forca4 from "../assets/forca4.jpg";
+import forca5 from "../assets/forca5.jpg";
+import forca6 from "../assets/forca6.jpg";
 
 const imagens = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 export default function Jogo({erros, iniciarJogo,palavraJogo, corPalavra}) {
